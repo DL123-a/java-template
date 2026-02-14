f168140bbed741c41d6d132ba86c26bf544b9328
DL123-a <dannylealgt@gmail.com>
2026-02-13 22:55:00 -0600

    Implementacion de pruebas unitarias

--- src/test/java/com/template/MainTest.java
+++ src/test/java/com/template/MainTest.java
@@ -23,4 +23,45 @@ public class MainTest {
             assertNotNull(main);
         });
     }
+
+    @Test
+    @DisplayName("BubbleSort ordena correctamente")
+    void testBubbleSort() {
+        Integer[] array = {5, 3, 8, 1, 2};
+        BubbleSort.sort(array);
+        assertArrayEquals(new Integer[]{1, 2, 3, 5, 8}, array);
+    }
+
+    @Test
+    @DisplayName("GnomeSort ordena correctamente")
+    void testGnomeSort() {
+        Integer[] array = {9, 4, 6, 2, 7};
+        GnomeSort.sort(array);
+        assertArrayEquals(new Integer[]{2, 4, 6, 7, 9}, array);
+    }
+
+    @Test
+    @DisplayName("MergeSort ordena correctamente")
+    void testMergeSort() {
+        Integer[] array = {10, 3, 5, 1, 8};
+        MergeSort.sort(array);
+        assertArrayEquals(new Integer[]{1, 3, 5, 8, 10}, array);
+    }
+
+    @Test
+    @DisplayName("QuickSort ordena correctamente")
+    void testQuickSort() {
+        Integer[] array = {7, 2, 9, 4, 1};
+        QuickSort.sort(array);
+        assertArrayEquals(new Integer[]{1, 2, 4, 7, 9}, array);
+    }
+
+    @Test
+    @DisplayName("RadixSort ordena correctamente")
+    void testRadixSort() {
+        Integer[] array = {170, 45, 75, 90, 802, 24, 2, 66};
+        RadixSort.sort(array);
+        assertArrayEquals(new Integer[]{2, 24, 45, 66, 75, 90, 170, 802}, array);
+    }
 }
+
